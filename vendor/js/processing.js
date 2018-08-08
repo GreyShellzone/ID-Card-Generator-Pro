f901936f6be27507401e4f48ee285578b94d64dc
Grey Shellzone <greyshellzone@gmail.com>
2018-08-08 22:53:20 +0200

    Update

--- vendor/js/processing.js
+++ vendor/js/processing.js
@@ -1,46 +1,20 @@
 
-var canvas = new fabric.Canvas('parent');
-// Define an array with all fonts
-var fonts = ["Pacifico", "VT323", "Quicksand", "Inconsolata"];
+var lastNumber = 50;
 
-var textbox = new fabric.Textbox('New text', {
-    left: 50,
-    top: 50,
-    width: 150,
-    fontSize: 20
-});
+function addTextBox() {
+    var canvas = new fabric.Canvas('parent');
+    // Define an array with all fonts
+    var fonts = ["Pacifico", "VT323", "Quicksand", "Inconsolata"];
 
-canvas.add(textbox).setActiveObject(textbox);
-fonts.unshift('Times New Roman');
-// Populate the fontFamily select
-var select = document.getElementById("font-family");
-
-fonts.forEach(function(font) {
-    var option = document.createElement('option');
-    option.innerHTML = font;
-    option.value = font;
-    select.appendChild(option);
-});
-
-// Apply selected font on change
-document.getElementById('font-family').onchange = function() {
-    if (this.value !== 'Times New Roman') {
-        loadAndUse(this.value);
-    } else {
-        canvas.getActiveObject().set("fontFamily", this.value);
-        canvas.requestRenderAll();
-    }
-};
-
-function loadAndUse(font) {
-    var myfont = new FontFaceObserver(font)
-    myfont.load()
-        .then(function() {
-            // when font is loaded, use it.
-            canvas.getActiveObject().set("fontFamily", font);
-            canvas.requestRenderAll();
-        }).catch(function(e) {
-        console.log(e)
-        alert('font loading failed ' + font);
+    var textbox = new fabric.Textbox('New text', {
+        left: lastNumber,
+        top: lastNumber,
+        width: 150,
+        fontSize: 20
     });
+
+    lastNumber += 10;
+    canvas.add(textbox).setActiveObject(textbox);
+    fonts.unshift('Times New Roman');
+    textbox = null;
 }

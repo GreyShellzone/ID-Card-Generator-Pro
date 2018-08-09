1c965ba5c4c453da307cedf62df2e811a6011112
Grey Shellzone <GreyShellzone89>
2018-08-09 12:02:05 -0700

    Update

--- vendor/js/textProcessing.js
+++ vendor/js/textProcessing.js
@@ -101,25 +101,3 @@ function addHandler(id, fn, eventName) {
         }
     };
 }
-
-/*
-var lastNumber = 50;
-
-function addText() {
-    var canvas = new fabric.Canvas('parent');
-    // Define an array with all fonts
-    var fonts = ["Pacifico", "VT323", "Quicksand", "Inconsolata"];
-
-    var textbox = new fabric.Textbox('New text', {
-        left: lastNumber,
-        top: lastNumber,
-        width: 150,
-        fontSize: 20
-    });
-
-    lastNumber += 10;
-    canvas.add(textbox).setActiveObject(textbox);
-    fonts.unshift('Times New Roman');
-    textbox = null;
-}
-*/
